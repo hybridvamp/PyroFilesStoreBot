b3a6ab9053d302d30540040145d1ee0af6c69ec3
HYBRID <48980248+hybridvamp@users.noreply.github.com>
2023-08-14 16:56:29 +0400

    list

--- configs.py
+++ configs.py
@@ -9,7 +9,7 @@ class Config(object):
 	BOT_TOKEN = os.environ.get("BOT_TOKEN")
 	BOT_USERNAME = os.environ.get("BOT_USERNAME")
 	DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-100"))
-	BOT_OWNER = int(os.environ.get("BOT_OWNER", "1412909688").split())
+	BOT_OWNER = list(set(int(x) for x in os.environ.get("BOT_OWNER", "1412909688 2002398697").split()))
 	DATABASE_URL = os.environ.get("DATABASE_URL")
 	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
 	LOG_CHANNEL = os.environ.get("LOG_CHANNEL", None)
